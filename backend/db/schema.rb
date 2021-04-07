5a6b33c8520cdcef148628ec5dbf6fde51266f4a
Itisafeature <thebigdebugger@gmail.com>
2021-04-07 17:36:24 -0400

    added name to score model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_07_213323) do
+ActiveRecord::Schema.define(version: 2021_04_07_213545) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_04_07_213323) do
     t.integer "movie_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["movie_id"], name: "index_scores_on_movie_id"
   end
 
