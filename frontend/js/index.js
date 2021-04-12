3a17fd980b410a337190ea6ffea1b01651b73b66
Itisafeature <thebigdebugger@gmail.com>
2021-04-12 19:26:29 -0400

    working on updating game

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,13 +1,38 @@
 const BASE_URL = 'http://localhost:3000/api';
 
 document.addEventListener('DOMContentLoaded', () => {
+  getMovie();
+});
+
+const getMovie = () => {
   fetch(`${BASE_URL}/random-movie`)
     .then(res => res.json())
     .then(data => updateTitle(data.data.attributes));
-
-  console.log('here');
-});
+};
 
 const updateTitle = movie => {
-  document.getElementsByClassName('title')[0].innerHTML = movie.title;
+  transformTitle(movie.title);
+};
+
+const transformTitle = title => {
+  for (let i = 0; i < title.length; i++) {
+    let elToAdd = document.createElement('p');
+
+    if (title[i].match(/[,'?!.]/)) {
+      elToAdd.classList.add('punctuation');
+      elToAdd.innerHTML = title[i];
+    } else if (title[i] === ' ') {
+      elToAdd.classList.add('space');
+      elToAdd.innerHTML = '&nbsp;';
+    } else {
+      elToAdd.classList.add('character');
+    }
+
+    document.getElementsByClassName('spots')[0].appendChild(elToAdd);
+    if (!elToAdd.classList.contains('space')) {
+      const spaceToAdd = document.createElement('p');
+      spaceToAdd.innerHTML = '&nbsp;';
+      document.getElementsByClassName('spots')[0].appendChild(spaceToAdd);
+    }
+  }
 };
